515c86051a1091e3cbda0763f402dfc0d7bb7dee
Michael LaBarbiera <mlaba49@gmail.com>
2021-01-04 11:20:27 -0500

    Tried adding a test

--- Palindrome.Testing/UnitTest1.cs
+++ Palindrome.Testing/UnitTest1.cs
@@ -1,4 +1,5 @@
 using System;
+using Palindrome.Domain;
 using Xunit;
 
 namespace Palindrome.Testing
@@ -6,9 +7,14 @@ namespace Palindrome.Testing
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        private void Test1()
         {
+            string input1 = "CIVIC";
+            string input2 = "MOO";
+            PalindromeChecker pc = new PalindromeChecker();
 
+            //Debug.Assert(pc.CheckPalindrome(input1));
+            //Debug.Assert(pc.CheckPalindrome(input2));
         }
     }
 }

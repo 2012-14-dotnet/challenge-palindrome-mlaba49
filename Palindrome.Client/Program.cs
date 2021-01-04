4028bf6d16a9cd25e60f36d9bbb2da7e87ab125f
Michael LaBarbiera <mlaba49@gmail.com>
2021-01-04 11:01:45 -0500

    More logic

--- Palindrome.Client/Program.cs
+++ Palindrome.Client/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Palindrome.Domain;
 
 namespace Palindrome.Client
 {
@@ -8,9 +9,17 @@ namespace Palindrome.Client
         {
             Boolean exit = false;
             string input;
+            PalindromeChecker myChecker = new PalindromeChecker();
             Console.WriteLine("Welcome to the palindrome program.");
             while(exit == false) {
                 Console.WriteLine("Please input a word or sentence.");
+                input = Console.ReadLine().ToUpper();
+
+                if(input != "") {
+                    if(myChecker.CheckPalindrome(input) == true) Console.WriteLine(input + " is a palindrome.");
+                    else Console.WriteLine(input + " is not a palindrome.");
+                }
+                else Console.WriteLine("You need to put something in, dingbat!");
 
                 Console.WriteLine("Would you like to exit? Type 'yes' to do so.");
                 input = Console.ReadLine().ToUpper();

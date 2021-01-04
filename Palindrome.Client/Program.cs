fbecb6610d2bffc120784c1f5a9da9f39f942c6d
Michael LaBarbiera <mlaba49@gmail.com>
2021-01-04 10:42:16 -0500

    Basic logic loop

--- Palindrome.Client/Program.cs
+++ Palindrome.Client/Program.cs
@@ -6,7 +6,16 @@ namespace Palindrome.Client
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Boolean exit = false;
+            string input;
+            Console.WriteLine("Welcome to the palindrome program.");
+            while(exit == false) {
+                Console.WriteLine("Please input a word or sentence.");
+
+                Console.WriteLine("Would you like to exit? Type 'yes' to do so.");
+                input = Console.ReadLine().ToUpper();
+                if(input == "YES") exit = true;
+            }
         }
     }
 }
